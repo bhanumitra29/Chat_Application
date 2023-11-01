67dca7e97c341953a2894926f9c929fcabab729e
Bhanumitra Thurpunati <bhanumithra296@gmail.com>
2023-11-02 00:37:47 +0530

    second commit

--- src/ClientCompo.js
+++ src/ClientCompo.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { IoIosChatbubbles } from 'react-icons/io';
-const socket = io('http://localhost:3001');
+const socket = io('https://chat-applicationapi.onrender.com');
 
 function ClientCompo() {
   const [name, setName] = useState('');
