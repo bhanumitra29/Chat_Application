5daa3d0efb89978b769a18a061958b78995f35dd
Bhanumitra Thurpunati <bhanumithra296@gmail.com>
2023-11-06 21:30:55 +0530

    second commit

--- src/ClientCompo.js
+++ src/ClientCompo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { IoIosChatbubbles } from 'react-icons/io';
+
 const socket = io('https://chat-applicationapi.onrender.com');
 
 function ClientCompo() {
@@ -8,39 +9,42 @@ function ClientCompo() {
   const [chatName, setChatName] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const joinChat = () => {
-    const personName = prompt('Enter the name of the chat person:');
+    const personName = prompt('Enter your name:');
     if (personName) {
       setName(personName);
       setChatName(personName);
-      socket.emit('join', personName);
-      alert('Joined the chat successfully'); 
+      socket.emit('joinChat', personName);
     }
   };
 
   const sendMessage = () => {
-    const newMessage = (
-      <div className="message-container">
-        <span className="chatName">{chatName}</span>
-        <div className="message">
-          <span className="bigFont">{message}</span>
-        </div>
-      </div>
-    );
-    
-    
-    
+    const newMessage = `${chatName}: ${message}`;
     socket.emit('chat message', newMessage);
     setMessage('');
-    setMessages([...messages, newMessage]);
   };
+  
 
-  useEffect(() => { 
+  useEffect(() => {
+    return () => {
+      socket.emit('user left', name);
+    };
+  }, [name]);
+
+  useEffect(() => {
     socket.on('chat message', (message) => {
       setMessages((prevMessages) => [...prevMessages, message]);
     });
 
+    socket.on('user joined', (user) => {
+      setUsers((prevUsers) => [...prevUsers, user]);
+    });
+
+    socket.on('user left', (user) => {
+      setUsers((prevUsers) => prevUsers.filter((u) => u !== user));
+    });
   }, []);
 
   return (
@@ -52,10 +56,17 @@ function ClientCompo() {
         </div>
       ) : (
         <div>
-          {/* <div> */}
-          <h1 className='Heading'> <IoIosChatbubbles className="chat-icon" /> {chatName}</h1>
-          {/* </div> */}
+          <h1 className='Heading'>
+            <IoIosChatbubbles className="chat-icon" /> {chatName}
+          </h1>
           <div className="chat-container">
+            <div className="user-list">
+              {/* <h2>Users in the chat:</h2> */}
+              {users.map((user, index) => (
+          <div key={index}>{user}</div>
+            ))}
+
+            </div>
             {messages.map((msg, index) => (
               <div key={index} className="message">
                 {msg}
